df4dfe9997526a4f58beefb3d9b02dc68fa604c3
Trevor Mast <trevormast@gmail.com>
2020-04-10 21:28:22 -0700

    adding chatroom index and forme

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,8 +3,23 @@ class ChatroomsController < ApplicationController
     @chatrooms = Chatroom.all
   end
 
+  def create
+    # intialize the chatroom
+    chatroom = Chatroom.new(chatroom_params)
+
+    # go back to index if save
+    redirect_to chatroom_path(chatroom.topic) and return if chatroom.save
+
+    # otherwise, go back to index
+    redirect_to chatrooms_path
+  end
+
   def show
     @chatroom = Chatroom.find_by(topic: params[:topic])
     @message = Message.new
+  end
+
+  def chatroom_params
+    params.require(:chatroom).permit(:topic)
   end
 end
