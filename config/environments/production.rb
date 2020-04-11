c779aa3b680234bd75b2015bb89e651d12c8eb97
Trevor Mast <trevormast@gmail.com>
2020-04-10 21:38:21 -0700

    prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://magic-merlin-chat.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://magic-merlin-chat.herokuapp.com', 'http://magic-merlin-chat.herokuapp.com']
 end
